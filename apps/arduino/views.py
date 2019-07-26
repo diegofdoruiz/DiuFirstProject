fa203241438688c31095e5bb9d9ed9eb2fdb3d92
diegopccali1 <dieg.fernando.ruiz@correounivalle.edu.co>
2019-07-26 15:25:43 -0500

    avance led

--- apps/arduino/views.py
+++ apps/arduino/views.py
@@ -1,20 +1,32 @@
 from django.shortcuts import render, redirect, get_object_or_404
 import serial
 import time
+from django.contrib import messages
 # Create your views here.
 
 def home(request):
 	return render(request, 'home.html', {})
 
 def test(request):
-	ser = serial.Serial()
-	ser.baudrate = 9600
-	ser.port = '/dev/cu.usbmodem14201'
-	ser.open()
-	time.sleep(2)
-	ser.write(b'2test')
-	time.sleep(2)
-	rawString = ser.readline()
-	print(rawString)
-	ser.close()
-	return render(request, 'arduino.html', {'ser':ser, 'respuesta':rawString})
+	if request.method == 'POST':
+		times = request.POST.get('times', 0)
+		print(times)
+		respuesta = "No se encenderá el bombillo"
+		if int(times) > 0:
+			ser = serial.Serial()
+			ser.baudrate = 9600
+			ser.port = '/dev/cu.usbmodem14201'
+			ser.open()
+			time.sleep(2)
+			send = bytearray()
+			send.extend(str(times).encode('latin-1'))
+			ser.write(send)
+			time.sleep(1)
+			respuesta = ser.readline()
+			messages.add_message(request, messages.INFO, respuesta.decode("utf-8"), 'respuesta')
+			ser.close()
+			return redirect('arduino:test')
+		return render(request, 'arduino.html', {'respuesta':respuesta})
+	else:
+		return render(request, 'arduino.html', {})
+	
